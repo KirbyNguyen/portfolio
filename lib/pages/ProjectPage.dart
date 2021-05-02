3244c1030de27c52966ab57db75509cdf133e1b7
KirbyNguyen <kirby.ducnguyen@outlook.com>
2021-05-01 23:38:20 -0700

    Basic Project Page components

--- lib/pages/ProjectPage.dart
+++ lib/pages/ProjectPage.dart
@@ -1,3 +1,4 @@
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 import 'package:portfolio/components/Navigation.dart';
 
@@ -32,13 +33,68 @@ class ProjectInformation extends StatelessWidget {
   Widget build(BuildContext context) {
     return Column(
       children: <Widget>[
-        Expanded(
+        Flexible(
           flex: 1,
           child: Center(
-            child: Text("Project Page"),
+            child: Text("More are coming"),
+          ),
+        ),
+        Expanded(
+          flex: 4,
+          child: SingleChildScrollView(
+            scrollDirection: Axis.horizontal,
+            child: Row(
+              children: <Widget>[
+                BudgetPrototype(),
+                SizedBox(width: 200),
+                BudgetPrototype(),
+              ],
+            ),
           ),
         ),
         Navigation(),
+      ],
+    );
+  }
+}
+
+class BudgetPrototype extends StatelessWidget {
+  const BudgetPrototype({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      // crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        Flexible(
+          flex: 1,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              Text(
+                "A BUDGET APP PROTOTYPE",
+                style: Theme.of(context).textTheme.bodyText2.copyWith(
+                      fontSize: 50.0,
+                    ),
+              ),
+              TextButton(
+                onPressed: () {},
+                child: Text("Learn More"),
+              ),
+            ],
+          ),
+        ),
+        Flexible(
+          flex: 3,
+          child: Image.asset(
+            'screenshotsBudget/1.png',
+            scale: 2.0,
+            alignment: Alignment.bottomCenter,
+          ),
+        ),
       ],
     );
   }
