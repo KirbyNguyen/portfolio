d15dbff2416d80723870338fa29686bef1c1a885
KirbyNguyen <kirby.ducnguyen@outlook.com>
2021-05-02 16:08:02 -0700

    Added Blob Page

--- lib/pages/BobPage.dart
+++ lib/pages/BobPage.dart
@@ -1,3 +1,4 @@
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 import 'package:portfolio/components/Navigation.dart';
 
@@ -17,13 +18,105 @@ class BobPage extends StatelessWidget {
             ],
           ),
         ),
-        child: Column(
-          children: <Widget>[
-            Text("Bob Page"),
-            Navigation(),
-          ],
-        ),
+        child: BobInformation(),
       ),
+    );
+  }
+}
+
+class BobInformation extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        Expanded(
+          flex: 2,
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Text(
+                  "BOB'S ADVENTURE",
+                  style: Theme.of(context).textTheme.bodyText2.copyWith(
+                        fontSize: 50.0,
+                      ),
+                ),
+                SizedBox(
+                  height: 20.0,
+                ),
+                RichText(
+                  text: TextSpan(
+                    text: "A platformer game built with ",
+                    style: Theme.of(context).textTheme.bodyText2.copyWith(
+                          letterSpacing: 5.0,
+                        ),
+                    children: [
+                      TextSpan(
+                        text: "GameMaker 2",
+                        style: Theme.of(context).textTheme.bodyText2.copyWith(
+                              color: Colors.green,
+                            ),
+                      ),
+                      WidgetSpan(
+                        child: Image.asset(
+                          'assets/gamemaker.png',
+                          height: 50.0,
+                          width: 50.0,
+                          alignment: Alignment.bottomCenter,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                SizedBox(height: 15),
+                Text(
+                  "This game was inspired by early Kirby games",
+                  style: Theme.of(context).textTheme.bodyText2.copyWith(
+                        letterSpacing: 3.0,
+                      ),
+                ),
+                SizedBox(height: 10),
+                Text(
+                  "Every sprite was drawn by me!",
+                  style: Theme.of(context).textTheme.bodyText2.copyWith(
+                        letterSpacing: 3.0,
+                      ),
+                ),
+                Text(
+                  "Three levels with three distinct enemy types",
+                  style: Theme.of(context).textTheme.bodyText2.copyWith(
+                        letterSpacing: 3.0,
+                      ),
+                ),
+                Text(
+                  "Collect fruits to increase points, and heal yourself!",
+                  style: Theme.of(context).textTheme.bodyText2.copyWith(
+                        letterSpacing: 3.0,
+                      ),
+                ),
+              ],
+            ),
+          ),
+        ),
+        Expanded(
+          flex: 3,
+          child: CarouselSlider(
+            options: CarouselOptions(),
+            items: [1, 2, 3, 4, 5, 6]
+                .map(
+                  (item) => Container(
+                    child: Center(
+                      child: Image.asset(
+                        'screenshotsBlob/$item.png',
+                      ),
+                    ),
+                  ),
+                )
+                .toList(),
+          ),
+        ),
+        Navigation(),
+      ],
     );
   }
 }
--- lib/pages/BudgetPage.dart
+++ lib/pages/BudgetPage.dart
@@ -1,3 +1,4 @@
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 import 'package:portfolio/components/Navigation.dart';
 
@@ -17,13 +18,117 @@ class BudgetPage extends StatelessWidget {
             ],
           ),
         ),
-        child: Column(
-          children: <Widget>[
-            Text("Budget Page"),
-            Navigation(),
-          ],
-        ),
+        child: BudgetInformation(),
       ),
+    );
+  }
+}
+
+class BudgetInformation extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        Expanded(
+          flex: 2,
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Text(
+                  "BUDGET APP PROTOTYPE",
+                  style: Theme.of(context).textTheme.bodyText2.copyWith(
+                        fontSize: 50.0,
+                      ),
+                ),
+                SizedBox(
+                  height: 20.0,
+                ),
+                RichText(
+                  text: TextSpan(
+                    text: "A budgeting app built with ",
+                    style: Theme.of(context).textTheme.bodyText2.copyWith(
+                          letterSpacing: 5.0,
+                        ),
+                    children: [
+                      TextSpan(
+                        text: "Flutter",
+                        style: Theme.of(context).textTheme.bodyText2.copyWith(
+                              color: Colors.blue,
+                            ),
+                      ),
+                      WidgetSpan(
+                        child: Image.asset(
+                          'assets/flutter_logo.png',
+                          height: 50.0,
+                          width: 50.0,
+                          alignment: Alignment.bottomCenter,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                RichText(
+                  text: TextSpan(
+                    text: "Supported by ",
+                    style: Theme.of(context).textTheme.bodyText2.copyWith(
+                          letterSpacing: 5.0,
+                        ),
+                    children: [
+                      TextSpan(
+                        text: "Firebase",
+                        style: Theme.of(context).textTheme.bodyText2.copyWith(
+                              color: Colors.red,
+                            ),
+                      ),
+                      WidgetSpan(
+                        child: Image.asset(
+                          'assets/firebase_logo.png',
+                          height: 50.0,
+                          width: 50.0,
+                          alignment: Alignment.bottomCenter,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                SizedBox(height: 15),
+                Text(
+                  "The user can create a category they want to check",
+                  style: Theme.of(context).textTheme.bodyText2.copyWith(
+                        letterSpacing: 3.0,
+                      ),
+                ),
+                SizedBox(height: 10),
+                Text(
+                  "They can also log in transactions (such as expense or income)",
+                  style: Theme.of(context).textTheme.bodyText2.copyWith(
+                        letterSpacing: 3.0,
+                      ),
+                ),
+              ],
+            ),
+          ),
+        ),
+        Expanded(
+          flex: 3,
+          child: CarouselSlider(
+            options: CarouselOptions(),
+            items: [1, 2, 3, 4, 5, 6, 7]
+                .map(
+                  (item) => Container(
+                    child: Center(
+                      child: Image.asset(
+                        'screenshotsBudget/$item.png',
+                      ),
+                    ),
+                  ),
+                )
+                .toList(),
+          ),
+        ),
+        Navigation(),
+      ],
     );
   }
 }
