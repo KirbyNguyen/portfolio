caf3bdb647c35479876686dbdb36c370a65fa434
KirbyNguyen <kirby.ducnguyen@outlook.com>
2021-05-01 21:56:34 -0700

    Finalized About Page

--- lib/pages/AboutPage.dart
+++ lib/pages/AboutPage.dart
@@ -2,16 +2,18 @@ import 'package:flutter/material.dart';
 import 'package:url_launcher/url_launcher.dart';
 import 'package:portfolio/components/Navigation.dart';
 
-void _launchURL() async {
-  const _resumeURL =
-      "https://drive.google.com/file/d/1sCUkxh_fjYYdt-Ga8pqSwymlYJ0fYUa6/view?usp=sharing";
-  if (await canLaunch(_resumeURL)) {
-    await launch(_resumeURL);
+const _resumeURL =
+    "https://drive.google.com/file/d/1sCUkxh_fjYYdt-Ga8pqSwymlYJ0fYUa6/view?usp=sharing";
+const _gitURL = "https://github.com/KirbyNguyen";
+const _linkedInURL = "https://www.linkedin.com/in/ducnguyen-kirby/";
+
+void _launchURL(String url) async {
+  if (await canLaunch(url)) {
+    await launch(url);
   } else {
-    throw 'Could not launch $_resumeURL';
+    throw 'Could not launch $url';
   }
 }
-
 
 class AboutPage extends StatelessWidget {
   @override
@@ -44,60 +46,109 @@ class AboutInformation extends StatelessWidget {
   Widget build(BuildContext context) {
     return Column(
       children: <Widget>[
+        Information(),
         Expanded(
           flex: 1,
-          child: Center(
-            child: Column(
-              children: <Widget>[
-                Text(
-                    "Hi! My name is Duc Nguyen. I am an undergrad at Cal State Fullerton."),
-                SizedBox(
-                  height: 20.0,
-                ),
-                Text(
-                    "I am a mobile developer with Flutter and Dart. I have also worked with React Native before."),
-                SizedBox(
-                  height: 20.0,
-                ),
-                Text(
-                    "I am also a self-taught game desinger. My engine of choose right now is GameMaker 2."),
-                SizedBox(
-                  height: 20.0,
-                ),
-                Text("I am proficient in Dart, JavaScript, C++, and Java"),
-                SizedBox(
-                  height: 20.0,
-                ),
-                Text("You can learn more about me through my resume"),
-                SizedBox(
-                  height: 20.0,
-                ),
-                ElevatedButton(
-                  onPressed: _launchURL,
-                  child: Text(
-                    "View Resume",
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 20.0,
+          child: Column(
+            children: <Widget>[
+              Text("My other links"),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                mainAxisSize: MainAxisSize.max,
+                children: <Widget>[
+                  GestureDetector(
+                    onTap: () {
+                      _launchURL(_gitURL);
+                    },
+                    child: Image.asset(
+                      'assets/GitHub-Logo.png',
+                      width: 75.0,
+                      height: 75.0,
+                      alignment: Alignment.bottomCenter,
                     ),
                   ),
-                  style: ButtonStyle(
-                    backgroundColor:
-                        MaterialStateProperty.all<Color>(Colors.red),
-                    padding: MaterialStateProperty.all<EdgeInsets>(
-                      EdgeInsets.symmetric(
-                        horizontal: 50.0,
-                        vertical: 15.0,
-                      ),
+                  SizedBox(width: 20.0),
+                  GestureDetector(
+                    onTap: () {
+                      _launchURL(_linkedInURL);
+                    },
+                    child: Image.asset(
+                      'assets/linkedin-talent.png',
+                      height: 100.0,
+                      width: 100.0,
+                      alignment: Alignment.bottomCenter,
                     ),
                   ),
-                ),
-              ],
-            ),
+                ],
+              ),
+            ],
           ),
         ),
         Navigation(),
       ],
+    );
+  }
+}
+
+class Information extends StatelessWidget {
+  const Information({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      flex: 2,
+      child: Center(
+        child: Column(
+          children: <Widget>[
+            Text(
+                "Hi! My name is Duc Nguyen. I am an undergrad at Cal State Fullerton."),
+            SizedBox(
+              height: 20.0,
+            ),
+            Text(
+                "I am a mobile developer with Flutter and Dart. I have also worked with React Native before."),
+            SizedBox(
+              height: 20.0,
+            ),
+            Text(
+                "I am also a self-taught game desinger. My engine of choose right now is GameMaker 2."),
+            SizedBox(
+              height: 20.0,
+            ),
+            Text("I am proficient in Dart, JavaScript, C++, and Java"),
+            SizedBox(
+              height: 20.0,
+            ),
+            Text("You can learn more about me through my resume"),
+            SizedBox(
+              height: 20.0,
+            ),
+            ElevatedButton(
+              onPressed: () {
+                _launchURL(_resumeURL);
+              },
+              child: Text(
+                "View Resume",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 20.0,
+                ),
+              ),
+              style: ButtonStyle(
+                backgroundColor: MaterialStateProperty.all<Color>(Colors.red),
+                padding: MaterialStateProperty.all<EdgeInsets>(
+                  EdgeInsets.symmetric(
+                    horizontal: 50.0,
+                    vertical: 15.0,
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
